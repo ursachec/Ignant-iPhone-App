ff0f7be9c0e632d150d49440af852e880faf7214
Claudiu-Vlad Ursache <ursachevlad@gmail.com>
2012-04-24 00:38:57 +0200

    added MosaicView class

---
+++ IgnantApp/MosaicView.h
@@ -0,0 +1,13 @@
+//
+//  MosaicView.h
+//  IgnantApp
+//
+//  Created by Claudiu-Vlad Ursache on 24.04.12.
+//  Copyright (c) 2012 c.v.ursache. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MosaicView : UIView
+
+@end
---
+++ IgnantApp/MosaicView.m
@@ -0,0 +1,31 @@
+//
+//  MosaicView.m
+//  IgnantApp
+//
+//  Created by Claudiu-Vlad Ursache on 24.04.12.
+//  Copyright (c) 2012 c.v.ursache. All rights reserved.
+//
+
+#import "MosaicView.h"
+
+@implementation MosaicView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
