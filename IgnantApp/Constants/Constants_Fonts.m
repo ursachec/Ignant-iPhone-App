5388ee806ff53a5fdd8ee28f66629e90d248a009
Claudiu-Vlad URsache <claudiu.vlad.ursache@gmail.com>
2012-05-07 21:31:03 +0200

    separated Constants into different files, added them to a "Constants" directory

---
+++ IgnantApp/Constants/Constants_Fonts.m
@@ -0,0 +1,11 @@
+//
+//  Constants_Fonts.m
+//  IgnantApp
+//
+//  Created by Claudiu-Vlad Ursache on 07.05.12.
+//  Copyright (c) 2012 c.v.ursache. All rights reserved.
+//
+
+#import "Constants_Fonts.h"
+
+
