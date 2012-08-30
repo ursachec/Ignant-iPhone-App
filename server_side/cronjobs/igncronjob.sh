dc478a0efbda1a6cd027b329efe0f53eb35527cc
Claudiu-Vlad Ursache <claudiu.vlad.ursache@gmail.com>
2012-08-30 14:29:53 +0200

    add slideshowImg generation to cronjob

--- server_side/cronjobs/igncronjob.sh
+++ server_side/cronjobs/igncronjob.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-echo '##########################################'
-echo 'starting ignant cronjob...'
-echo '##########################################'
 
-PHP_SCRIPT_LOG="script.log"
+PHP_SCRIPT_LOG="/www/htdocs/w00d3020/ignantblog/app/cronjobs/script.log"
 
 MODULES_DIR="/www/htdocs/w00d3020/ignantblog/app/modules"
 MOSAIC_SCRIPT_DIR="$MODULES_DIR/mosaic/mosaic_generator.php"
@@ -13,16 +10,22 @@ NOTIFICATIONS_SCRIPT_DIR="$MODULES_DIR/"
 BATCH=20
 SLEEP_FOR=1
 
-php $MOSAIC_SCRIPT_DIR max=$BATCH ;
-sleep $SLEEP_FOR ;
-php $THUMB_SCRIPT_DIR imgType=detailImg max=$BATCH ;
-sleep $SLEEP_FOR ;
-php $THUMB_SCRIPT_DIR imgType=mosaicImg max=$BATCH ;
-sleep $SLEEP_FOR ;
-php $THUMB_SCRIPT_DIR imgType=relatedImg max=$BATCH ;
+NOW=$(date)
 
+echo "\n##########################################" >> $PHP_SCRIPT_LOG
+echo "starting ignant cronjob... ($NOW)" >> $PHP_SCRIPT_LOG
+echo '##########################################' >> $PHP_SCRIPT_LOG
 
-echo '##########################################'
-echo 'finished running ignant cronjob.'
-echo '##########################################'
+php $MOSAIC_SCRIPT_DIR max=$BATCH >> $PHP_SCRIPT_LOG ;
+sleep $SLEEP_FOR ;
+php $THUMB_SCRIPT_DIR imgType=detailImg max=$BATCH >> $PHP_SCRIPT_LOG ;
+sleep $SLEEP_FOR ;
+php $THUMB_SCRIPT_DIR imgType=mosaicImg max=$BATCH >> $PHP_SCRIPT_LOG ;
+sleep $SLEEP_FOR ;
+php $THUMB_SCRIPT_DIR imgType=relatedImg max=$BATCH >> $PHP_SCRIPT_LOG ;
+sleep $SLEEP_FOR ;
+php $THUMB_SCRIPT_DIR imgType=slideshowImg max=$BATCH >> $PHP_SCRIPT_LOG ;
 
+echo '##########################################' >> $PHP_SCRIPT_LOG
+echo 'finished running ignant cronjob.' >> $PHP_SCRIPT_LOG
+echo "##########################################\n" >> $PHP_SCRIPT_LOG
