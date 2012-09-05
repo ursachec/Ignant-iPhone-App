b0b0b75410753d4eb77986f09a394db4afae8a75
Claudiu-Vlad Ursache <claudiu.vlad.ursache@gmail.com>
2012-09-05 19:00:19 +0200

    add new UIView category

---
+++ iphone_app/Categories/Views/UIView+PositioningUtils.h
@@ -0,0 +1,13 @@
+//
+//  UIView+PositioningUtils.h
+//  IgnantApp
+//
+//  Created by Claudiu-Vlad Ursache on 05.09.12.
+//  Copyright (c) 2012 c.v.ursache. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (PositioningUtils)
+
+@end
---
+++ iphone_app/Categories/Views/UIView+PositioningUtils.m
@@ -0,0 +1,18 @@
+//
+//  UIView+PositioningUtils.m
+//  IgnantApp
+//
+//  Created by Claudiu-Vlad Ursache on 05.09.12.
+//  Copyright (c) 2012 c.v.ursache. All rights reserved.
+//
+
+#import "UIView+PositioningUtils.h"
+
+@implementation UIView (PositioningUtils)
+
+-(void)addSubviewCentered:(UIView *)view
+{
+	[self addSubview:view];	
+}
+
+@end
